9a0cdf4f959fa4d9197bb2cc988ec7f630159c9e
cstapler <cstapler@iastate.edu>
2016-04-15 14:09:56 -0500

    Renames and gets into a functional state for linux host

--- tkGuiDreamMachine.py
+++ tkGuiDreamMachine.py
@@ -8,46 +8,47 @@ import serial
 import io
 
 top = Tkinter.Tk()
-#ser = serial.Serial() #Serial instance
-#ser.baudrate = 57600 #Specifies baudrate
-#ser.stopbits = STOPBITS_TWO #Specifies number of stopbits(Has associated constants)
-#ser.timeout = 20 #Specifies timeout in number of seconds(for read)
-#ser.port = 'COM8' #Specifies which port to connect (On Windows 'COM#' format should work)
+ser = serial.Serial() #Serial instance
+ser.baudrate = 57600 #Specifies baudrate
+ser.stopbits = 2 #Specifies number of stopbits(Has associated constants)
+ser.timeout = 20 #Specifies timeout in number of seconds(for read)
+ser.port = '/dev/rfcomm0' #Specifies which port to connect (On Windows 'COM#' format should work)
 
-#ser.open() #opens port communication
-#ser.is_open
+ser.open() #opens port communication
+print ser.is_open
 
 def moveBot():
-    #ser.write("1")
+    print "1 Should be written to bot"
+    ser.write("1")
     tkMessageBox.showinfo("Dream Machine", "Move")
-    
+
 def scanWithBot():
-    #ser.write("2")
+    print "2 Should be written to bot"
+    ser.write("2")
     tkMessageBox. showinfo("Dream Machine","Scan")
-    
-def rotateBot(angle):
-    #ser.write("3 %d" % (angle))
+
+def rotateBot():
+    print "3 should be written to bot"
+    ser.write("3")
     tkMessageBox.showinfo("Dream Machine", "Rotate")
-    
+
 #Button Linked to Moving Bot Forward
 B1 = Tkinter.Button(top, text = "Move Robot Forward", command = moveBot)
 B1.grid(row = 0, column = 0)
 #Button linked to Scanning with Bot
 B2 = Tkinter.Button(top, text = "Scan with Robot", command = scanWithBot)
 B2.grid(row = 3, column = 0)
-#Label for angle used in rotate function                   
+#Label for angle used in rotate function
 angleL = Label(top, text="Enter Angle")
 angleL.grid(row = 6, column = 0)
-#Entry field for angle used rotate Function 
+#Entry field for angle used rotate Function
 angleE = Entry(top,bd = 5)
 angleE.grid(row = 6, column = 1)
 #Button linked to rotating bot a certain amount specified in angle Entry
-B3 = Tkinter.Button(top, text = "Rotate Robot", command = rotateBot(angleE.get()))
+B3 = Tkinter.Button(top, text = "Rotate Robot", command = rotateBot)
 B3.grid(row = 9, column = 0)
-
-
 
 top.mainloop()
 
 ser.close() #closes port
-ser.is_open
+print ser.is_open
